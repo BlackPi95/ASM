4e1e5f6140e60898763d577e98bf0f53a8884777
VTH <haivtgch17008@fpt.edu.vn>
2019-05-03 21:58:56 +0700

    ádas

--- deleteCate.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	require_once('./dbconnector.php');
-
-	$ID=$_GET['CateID'];
-    $sql="DELETE FROM categories WHERE CateID = $ID";
-    $cn = new DBConnector();
-    $return = $cn->execStatement($sql);    
-    header('location:ManagerCate.php'); 
- ?>
